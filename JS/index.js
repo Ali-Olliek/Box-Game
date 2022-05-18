60fa8ed6aa29825bead750b77195b82917f82f5c
AliOlliek <olliek95@gmail.com>
2022-05-18 19:17:37 +0300

    Wrote pseudo code for JS

---
+++ JS/index.js
@@ -0,0 +1,19 @@
+// Game Layout - Psuedo Code
+
+// Event: "Spacebar" --> Playing=True;
+// While Playing {}
+// --- Boxes-list = [box-1,box-2,box-3,box-4]
+// --- Randomly select a number from 0 to 4, this will be a get-box-index
+// --- Boxes-list[get-box-index]
+// --- Add number to played-boxes-list = [...]
+// --- Play-boxes {}
+// ------ do animation of boxes for a length of played-boxes-list
+// ------ index of the box:
+// ---------Changes: plays sound, changes color to grey
+// --- Eventlistener: click on boxes:
+// --- add box-clicked to boxes-guessed [...]
+// --- if boxes-guessed == played-boxes-list:
+// --- Playing = True
+// --- Level + 1
+// --- (We then start again but instead of zero-ing the played-boxes-list we append to it)
+// --- else: playing = False. Display lose animation (red background, display You Lost! message)
