7879a6d9d372f433d490bacf00d7f4b94f81948d
AliOlliek <olliek95@gmail.com>
2022-05-22 20:56:55 +0300

    Creating final structure of the program without functionality

--- JS/index.js
+++ JS/index.js
@@ -31,13 +31,16 @@ var box1 = document.getElementById('box1')
 var box2 = document.getElementById('box2')
 var box3 = document.getElementById('box3')
 var box4 = document.getElementById('box4')
+var game_prompt= document.getElementById('game_prompt')
 var audio1 = new Audio ("Assets/sounds/sounds/red.mp3")
 var audio2 = new Audio ("Assets/sounds/sounds/green.mp3")
 var audio3 = new Audio ("Assets/sounds/sounds/blue.mp3")
 var audio4 = new Audio ("Assets/sounds/sounds/yellow.mp3")
-var playing = false;
 var random_index = get_random_index()
 var boxes_list = ['box1','box2','box3','box4']
+var boxes_list_2 = document.getElementsByClassName('box')
+level = 0;
+
 var played_boxes = [];
 
 var choose_box = function() {
@@ -47,64 +50,69 @@ var choose_box = function() {
     return played_boxes
 }
 
+var play_box = function() {
+        for(let i=0;i<played_boxes.length;i++){ 
 
-var play_box = function(){
-        for(let i=0;i<played_boxes.length;i++){
-            box4.setAttribute("class", "box box-4");
-            box3.setAttribute("class", "box box-3");
-            box2.setAttribute("class", "box box-2");
-            box1.setAttribute("class", "box box-1");
-
-            console.log('outside')
-
-            if(played_boxes[i]=='box1'){
-                setTimeout(() => {
-                box1.setAttribute("class", "box grey_box");
+            if (played_boxes[i] == "box1") {
+              box1.setAttribute("class", "box box-1 grey_box");
+              setTimeout(() => {
+                box1.classList.remove("grey_box");
                 audio1.play();
-                box1.removeAttribute("grey_box");
-                }, 1000*i);
-                
-        }
-
-            console.log("in timeout")
-            if (played_boxes[i] == "box2") {
-                setTimeout(() => {
-                box2.setAttribute("class", "box grey_box");
+              }, (500 * i) / 5 + i);
+            } 
+            else if (played_boxes[i] == "box2") {
+              box2.setAttribute("class", "box box-2 grey_box");
+              setTimeout(() => {
+                box2.classList.remove("grey_box");
                 audio2.play();
-                box1.removeAttribute("grey_box");
-                }, 1000*i);
-                
-        }
-            
-            if (played_boxes[i] == "box3") {
-                setTimeout(() => {
-                box3.setAttribute("class", "box grey_box");
+              }, (500 * i) / 5 + i);
+            } 
+            else if (played_boxes[i] == "box3") {
+              box3.setAttribute("class", "box box-3 grey_box");
+              setTimeout(() => {
+                box3.classList.remove("grey_box");
                 audio3.play();
-                box1.removeAttribute("grey_box");
-                }, 1000*i);
-                
-        }
+              }, 500 * i);
+            }
 
-            if (played_boxes[i] == "box4") {
-                setTimeout(() => {
-                box4.setAttribute("class", "box grey_box");
-                audio3.play();
-                box1.removeAttribute("grey_box");
-                }, 1000*i);
-                
+            else {
+                box4.setAttribute("class", "box box-4 grey_box");
+                setTimeout(() => {box4.classList.remove("grey_box");
+                audio4.play();}, 500 * i/5+i);
         }
-            console.log("this is i:  " +i)
-            
+        
         }
+}
+
+var user_plays = function () {
+    for (var i = 0; i < boxes_list_2.length; i++) {
+      boxes_list_2[i].addEventListener("click", check_pattern)
     }
+} 
+
+var check_pattern = function() {
+    boxes_guessed = []
+    boxes_guessed.push(user_plays())
+    console.log(boxes_guessed)
+
+}
+
+var you_won = function() {
+    level++
+    document.body.style.backgroundColor = "green";
+    game_prompt.innerHTML = "YOU WON!"+level;
+}
+
+var you_lost = function () {
+    message.innerHTML = "YOU LOST! PRESS SPACEBAR TO RESET";
+    document.body.style.backgroundColor = "red";
+}
 
 document.addEventListener("keyup", (e) => { 
     if (e.code === "Space") {
         choose_box()
-        play_box()
         console.log(played_boxes)
+        play_box()
+        user_plays()
+
 }})
-
-    clearInterval(boxes_sounds);
-
-    
